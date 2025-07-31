87dd38d13b1f6f0504646c13a3d7293f2073aede
anton <anton.loncar@outlook.com>
2025-07-31 13:16:54 +0200

    used useEffect to send http request after updating redux store

--- ReduxCart/src/App.js
+++ ReduxCart/src/App.js
@@ -3,10 +3,20 @@ import { useSelector } from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
 
 function App() {
 
   const showCart = useSelector(state => state.ui.cartIsVisible);
+
+  const cart = useSelector(state => state.cart);
+
+  useEffect(() => {
+    fetch("https://redux-cart-77823-default-rtdb.europe-west1.firebasedatabase.app/cart.json", {
+       method: "PUT",
+       body: JSON.stringify(cart)
+      });
+  }, [cart])
 
   return (
     <Layout>
